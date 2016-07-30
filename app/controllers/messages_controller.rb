547489bb6953968403f7cb0fd46e6b4e927c08f3
natron19 <nathan19@hotmail.com>
2016-07-29 21:15:55 -0700

    add user id to messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,7 +14,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.build(message_params)
 
     if @message.save
       redirect_to root_path
